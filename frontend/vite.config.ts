c50c01f27cbeb255cbc0be32cbdc95d7d7918d6c
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2026-02-23 19:18:22 -0300

    fix: corrigindo diretorio de saida e configuracao base para Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gamekis/',
+  // Se estiver na Vercel, o base é '/', se for GitHub Pages, é '/gamekis/'
+  base: process.env.VERCEL ? '/' : '/gamekis/',
 })
